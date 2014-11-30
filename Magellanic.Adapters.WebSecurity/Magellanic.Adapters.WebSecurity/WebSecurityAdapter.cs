cd14e64cf4f4ba71236f284725b154a26bba4c80
Jeremy_Lindsay_cp <SND\Jeremy_Lindsay_cp>
2014-11-30 18:40:22 +0000

    Initial Check-in. See https://websecurityadapter.codeplex.com for project details. Contains only the basic wrappers for creating a user, logging on a user, and logging a user out.

---
+++ Magellanic.Adapters.WebSecurity/Magellanic.Adapters.WebSecurity/WebSecurityAdapter.cs
@@ -0,0 +1,49 @@
+﻿namespace Magellanic.Adapters.WebSecurity
+{
+    using Magellanic.Adapter.WebSecurity;
+    using WebMatrix.WebData;
+
+    public class WebSecurityAdapter : IWebSecurityAdapter
+    {
+        /// <summary>
+        /// Logs the user out.
+        /// </summary>
+        public void LogUserOut()
+        {
+            WebSecurity.Logout();
+        }
+
+        /// <summary>
+        /// Creates the user and account.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        public void CreateUserAndAccount(string userName, string password)
+        {
+            WebSecurity.CreateUserAndAccount(userName, password);
+        }
+
+        /// <summary>
+        /// Logs the user on.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        public bool LogUserOn(string userName, string password)
+        {
+            return WebSecurity.Login(userName, password);
+        }
+
+        /// <summary>
+        /// Logs the user on.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="rememberMe">if set to <c>true</c> [remember me].</param>
+        /// <returns></returns>
+        public bool LogUserOn(string userName, string password, bool rememberMe)
+        {
+            return WebSecurity.Login(userName, password, persistCookie: rememberMe);
+        }
+    }
+}
