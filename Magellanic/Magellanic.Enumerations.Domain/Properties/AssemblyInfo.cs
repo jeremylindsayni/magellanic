13b4320641bfa3666f4b0424d2434b556b08b555
Jeremy_Lindsay_cp <jeremylindsayni@users.noreply.github.com>
2014-12-22 01:09:42 +0000

    Created a project for enumerations. Currently this only contains a single enumeration of Gender.

---
+++ Magellanic/Magellanic.Enumerations.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Magellanic.Enumerations.Domain")]
+[assembly: AssemblyDescription("A collection of enumerations for common domain objects.")]
+[assembly: AssemblyConfiguration("Release configuration, using .NET framework 4.0")]
+[assembly: AssemblyCompany("Jeremy Lindsay, 2014")]
+[assembly: AssemblyProduct("Magellanic.Enumerations.Domain")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be9bc767-1544-4c3a-8fd2-a1949ebb8d1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.*")]
---
+++ Magellanic/Magellanic.Enumerations.Domain/Users/Gender.cs
@@ -0,0 +1,28 @@
+﻿// ***********************************************************************
+// Assembly         : Magellanic.Enumerations.Domain
+// Author           : Jeremy Lindsay
+// Created          : 21-Dec-2014
+//
+// Last Modified By : Jeremy Lindsay
+// Last Modified On : 21-Dec-2014
+// ***********************************************************************
+/// <summary>
+/// The Users namespace.
+/// </summary>
+namespace Magellanic.Enumerations.Domain.Users
+{
+    /// <summary>
+    /// The Gender enumeration.
+    /// </summary>
+    public enum Gender
+	{
+        /// <summary>
+        /// The male gender type.
+        /// </summary>
+        Male = 0,
+        /// <summary>
+        /// The female gender type.
+        /// </summary>
+        Female = 1
+	} 
+}
