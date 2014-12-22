02f6d2df4baa82a40a492769aacccf1612d73c43
Jeremy_Lindsay_cp <SND\Jeremy_Lindsay_cp>
2014-12-22 01:14:39 +0000

    Added a project to hold business domain objects.

---
+++ Magellanic/Magellanic.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Magellanic.Domain")]
+[assembly: AssemblyDescription("A collection common domain objects.")]
+[assembly: AssemblyConfiguration("Release configuration, using .NET framework 4.0")]
+[assembly: AssemblyCompany("Jeremy Lindsay, 2014")]
+[assembly: AssemblyProduct("Magellanic.Domain")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b58b007-ddae-4549-8f64-6210f99ffd1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.*")]
---
+++ Magellanic/Magellanic.Domain/Security/Credentials.cs
@@ -0,0 +1,33 @@
+﻿// ***********************************************************************
+// Assembly         : Magellanic.Domain
+// Author           : Jeremy Lindsay
+// Created          : 22-Dec-2014
+//
+// Last Modified By : Jeremy Lindsay
+// Last Modified On : 22-Dec-2014
+// ***********************************************************************
+/// <summary>
+/// The Security namespace.
+/// </summary>
+namespace Magellanic.Domain.Security
+{
+    using Magellanic.Interfaces.Domain.Security;
+
+    /// <summary>
+    /// The Credentials class.
+    /// </summary>
+    public class Credentials : ICredentials
+    {
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>The password.</value>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>The name of the user.</value>
+        public string UserName { get; set; }
+    }
+}
